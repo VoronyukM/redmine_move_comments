74080580a40924dc786304f93044c59d7776fb9c
Mikhail Voronyuk <Voronyuk.M@3soft.ru>
2015-11-26 16:45:13 +0300

    new_issue_id field and moving to another issue

--- lib/move_comments_hooks.rb
+++ lib/move_comments_hooks.rb
@@ -8,5 +8,20 @@ class HelpdeskHooks < Redmine::Hook::Listener
     )
   end
  
+  # fetch 'new_issue_id' param and move the journal to another issue if presented
+  def controller_journals_edit_post(context={})
+    new_issue_id = context[:params]['new_issue_id']
+    if new_issue_id.present?
+      current_journal = context[:journal]
+      journal = Journal.new(:journalized_id => new_issue_id, :journalized_type => current_journal.journalized_type, :user_id => current_journal.user_id, :notes => current_journal.notes, :private_notes => current_journal.private_notes, :created_on => current_journal.created_on)
+      journal.save
+      # erase notes for current journal or delete it if it has no details
+      if current_journal.details.empty?
+        current_journal.destroy
+      else
+        current_journal.update_attributes(:notes => nil)
+      end
+    end
+  end
 
 end
