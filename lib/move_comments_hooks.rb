6402cffa8c179b4149419f66c1a0c7a0022c59c7
Mikhail Voronyuk <Voronyuk.M@3soft.ru>
2015-11-27 00:19:14 +0300

    show error if new issue is wrong

--- lib/move_comments_hooks.rb
+++ lib/move_comments_hooks.rb
@@ -13,7 +13,20 @@ class HelpdeskHooks < Redmine::Hook::Listener
     new_issue_id = context[:params]['new_issue_id']
     if new_issue_id.present?
       current_journal = context[:journal]
-      journal = Journal.new(:journalized_id => new_issue_id, :journalized_type => current_journal.journalized_type, :user_id => current_journal.user_id, :notes => current_journal.notes, :private_notes => current_journal.private_notes, :created_on => current_journal.created_on)
+      issue_id = nil
+      begin
+        issue = Issue.find(new_issue_id)
+        if issue
+          issue_id = issue.id
+        end
+      rescue
+      end
+      if !issue_id
+        context[:journal].class.module_eval { attr_accessor :wrong_new_issue_id} 
+        context[:journal].wrong_new_issue_id = new_issue_id # this will be analyzed later for showing the error message
+	return
+      end
+      journal = Journal.new(:journalized_id => issue_id, :journalized_type => current_journal.journalized_type, :user_id => current_journal.user_id, :notes => current_journal.notes, :private_notes => current_journal.private_notes, :created_on => current_journal.created_on)
       journal.save
       # erase notes for current journal or delete it if it has no details
       if current_journal.details.empty?
@@ -23,5 +36,16 @@ class HelpdeskHooks < Redmine::Hook::Listener
       end
     end
   end
+  
+  def view_journals_update_js_bottom(context={})
 
+    wrong_new_issue_id = context[:journal].wrong_new_issue_id
+    if !wrong_new_issue_id.nil?
+      action_view = ActionView::Base.new(File.dirname(__FILE__) + '/../app/views/')
+      action_view.render(
+      :partial => "notes_error"
+      ) 
+    end
+  end
+ 
 end
