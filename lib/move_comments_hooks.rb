f21f318d06fc6c360286a09ab33ff9b49b9f132c
Mikhail Voronyuk <Voronyuk.M@3soft.ru>
2015-11-27 13:11:21 +0300

    class name fixed

--- lib/move_comments_hooks.rb
+++ lib/move_comments_hooks.rb
@@ -1,4 +1,4 @@
-class HelpdeskHooks < Redmine::Hook::Listener
+class MoveCommentsHooks < Redmine::Hook::Listener
   
   # render partial for 'Move the comment to another issue'
   def view_journals_notes_form_after_notes(context={})
